dd69b03e1d0d66e6d8bbe2bad99b8fdc5e166d45
Mark Rathbone <markanthonyrathbone@gmail.com>
2020-08-30 18:37:43 +0100

    Version 0.1.2

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Generates random characters.
 
+Version 0.1.2: Caps Lock being hit implemented. Shift now just counts length of string to determine odds.
+
 Version 0.1.1: Shift-characters should be 2% rather than 50%
+
 Version 0.1: Initial commit.
--- monkey.py
+++ monkey.py
@@ -1,20 +1,32 @@
+import random
+
 f = open("output.txt", "a")
 f.truncate(0)
 
-import random
-
-one = "qwertyuiopasdfghjklzxcvbnm1234567890-=[];'#,./`\\ "
-shift = "QWERTYUIOPASDFGHJKLZXCVBNM ¬!\"£$%^&*()_+{}:@~<>?| "
+#All characters our monkey can press by pressing one key in one. All characters that require shift in shift.
+one = "qwertyuiopasdfghjklzxcvbnm1234567890-=[];'#,./`\\ \n"
+shift = "QWERTYUIOPASDFGHJKLZXCVBNM ¬!\"£$%^&*()_+{}:@~<>?| \n"
+#Capslock
+capsOne = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890-=[];'#,./`\\ \n"
+capsShift = "qwertyuiopasdfghjklzxcvbnm1 ¬!\"£$%^&*()_+{}:@~<>?| \n"
+capsLock = False
 
 n = 0
 
 while n in range (0,999):
-    rnum = random.randint(0,49)
-    #There's a 1 in 50 chance the first selection by the monkey will be shift. 
-    if rnum in range (0,49):
-        f.write (random.choice(one))
-    if rnum == 49:
-        f.write (random.choice(shift))
+    rnum = random.randint(0,int(len(one)))
+    if rnum == (int(len(one))):
+        capsLock = not capsLock
+    if capsLock == False:
+        if rnum in range (0,(int(len(one)-2))):
+            f.write (random.choice(one))
+        if rnum == (int(len(one)-1)):
+            f.write (random.choice(shift))
+    else:
+        if rnum in range (0,(int(len(one)-2))):
+            f.write (random.choice(capsOne))
+        if rnum == (int(len(one)-1)):
+            f.write (random.choice(capsShift))
     n = n+1
 
 f.write ("\n")
