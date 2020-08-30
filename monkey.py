619fb47e461c2242ba9445d26c9c6b496df0e2ed
Mark Rathbone <markanthonyrathbone@gmail.com>
2020-08-30 18:12:38 +0100

    Version 0.1.1

--- monkey.py
+++ monkey.py
@@ -2,17 +2,19 @@ f = open("output.txt", "a")
 f.truncate(0)
 
 import random
-import string
 
-one = "qwertyuiopasdfghjklzxcvbnm1234567890-=[];'#,./`\\"
-shift = "QWERTYUIOPASDFGHJKLZXCVBNM ¬!\"£$%^&*()_+{}:@~<>?|"
+one = "qwertyuiopasdfghjklzxcvbnm1234567890-=[];'#,./`\\ "
+shift = "QWERTYUIOPASDFGHJKLZXCVBNM ¬!\"£$%^&*()_+{}:@~<>?| "
 
 n = 0
 
 while n in range (0,999):
-    rnum = random.randint(0,2)
-    if rnum in range (0,1):
+    rnum = random.randint(0,49)
+    #There's a 1 in 50 chance the first selection by the monkey will be shift. 
+    if rnum in range (0,49):
         f.write (random.choice(one))
-    if rnum == 2:
+    if rnum == 49:
         f.write (random.choice(shift))
     n = n+1
+
+f.write ("\n")
